5fa0404a4528468d650908f0f29e3b5f021bcc1c
Karol Siezieniewski <karol@jubilex.com.pl>
2021-12-30 17:22:01 +0100

    Improving healthcheck for rest controller

--- src/main/java/pl/example/quiz/rest/HealthcheckRestController.java
+++ src/main/java/pl/example/quiz/rest/HealthcheckRestController.java
@@ -3,13 +3,15 @@ package pl.example.quiz.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.example.quiz.dto.HealthcheckDto;
 
 @RestController
 @RequestMapping("/api/health")
 public class HealthcheckRestController {
 
     @GetMapping
-    public String healthcheck() {
-        return "Healthcheck positive!";
+    public HealthcheckDto healthcheck() {
+        HealthcheckDto healthcheckDto = new HealthcheckDto(true, "Healthcheck is positive!");
+        return healthcheckDto;
     }
 }
