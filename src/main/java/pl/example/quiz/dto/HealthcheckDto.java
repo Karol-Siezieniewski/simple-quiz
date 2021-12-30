f8193d5bfbc236ba710820191a01bb10c44eaa9c
Karol Siezieniewski <karol@jubilex.com.pl>
2021-12-30 17:24:45 +0100

    Adding lombok implementation to healthcheck for rest controller

--- src/main/java/pl/example/quiz/dto/HealthcheckDto.java
+++ src/main/java/pl/example/quiz/dto/HealthcheckDto.java
@@ -1,21 +1,12 @@
 package pl.example.quiz.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class HealthcheckDto {
 
     private boolean status;
     private String message;
-
-    public HealthcheckDto(boolean status, String message) {
-        this.status = status;
-        this.message = message;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
